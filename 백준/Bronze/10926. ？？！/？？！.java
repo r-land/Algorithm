75c1d41f6d301f68eee1e957f644e3f0230d6a66
조아름 <121758937+r-land@users.noreply.github.com>
2023-03-20 19:05:05 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String last = "??!";
+        String S = sc.next();
+        System.out.println(S + last);
+        sc.close();
+    }
+}
